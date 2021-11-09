75e1bf0ed6774a2b090dc87f8292dfeff8e15603
webdeveloper510 <developerweb6@gmail.com>
2021-11-10 03:34:08 +0530

    admin.php

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -9,7 +9,7 @@ class Admin extends BaseController
 
     public function __construct()
     {
-        parent::__construct(); 
+        //parent::__construct(); 
 
         $model = new Admin();    
     }
