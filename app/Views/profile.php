449de48cad5575812851d30cf0ef901faa5fd129
webdeveloper510 <developerweb6@gmail.com>
2021-11-10 22:07:29 +0530

    profile.php

--- app/Views/profile.php
+++ app/Views/profile.php
@@ -9,14 +9,14 @@
   <meta name="author" content="Creative Tim">
   <title>Argon Dashboard - Free Dashboard for Bootstrap 4</title>
   <!-- Favicon -->
-  <link rel="icon" href="../assets/img/brand/favicon.png" type="image/png">
+  <link rel="icon" href="./assets/img/brand/favicon.png" type="image/png">
   <!-- Fonts -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
   <!-- Icons -->
-  <link rel="stylesheet" href="../assets/vendor/nucleo/css/nucleo.css" type="text/css">
-  <link rel="stylesheet" href="../assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" type="text/css">
+  <link rel="stylesheet" href="./app/assets/vendor/nucleo/css/nucleo.css" type="text/css">
+  <link rel="stylesheet" href="./app/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" type="text/css">
   <!-- Argon CSS -->
-  <link rel="stylesheet" href="../assets/css/argon.css?v=1.2.0" type="text/css">
+  <link rel="stylesheet" href="./app/assets/css/argon.css?v=1.2.0" type="text/css">
 </head>
 
 <body>
